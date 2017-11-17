eb3072eb8ea87fc8a5ed1f91d52f9c2cc16718d6
WoodHoodie <33759241+WoodHoodie@users.noreply.github.com>
2017-11-17 17:13:53 +0000

    LEDs now red

--- ContollerLED/main/__init__.py
+++ ContollerLED/main/__init__.py
@@ -4,7 +4,7 @@ import sys
 from urllib.request import localhost
 
 
-led_colours= [(0 ,0 ,0)] * 360
+led_colours= [(255 ,0 ,0)] * 360
 client= opc.Client("localhost:7890")
 
 client.put_pixels(led_colours, 0)
